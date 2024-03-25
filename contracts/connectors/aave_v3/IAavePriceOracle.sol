f3cb974ee2d4cc6da71c2367f972024fe25f9a91
Piotr Rzonsowski <pete@ipor.io>
2024-03-25 14:25:14 +0100

    init

--- contracts/connectors/aave_v3/IAavePriceOracle.sol
+++ contracts/connectors/aave_v3/IAavePriceOracle.sol
@@ -6,6 +6,8 @@ interface IAavePriceOracle {
      * @notice Returns the asset price in the base currency
      * @param asset The address of the asset
      * @return The price of the asset
+     * @dev address on Ethereum Mainnet 0x54586bE62E3c3580375aE3723C145253060Ca0C2
+     * @dev https://docs.aave.com/developers/core-contracts/aaveoracle
      */
     function getAssetPrice(address asset) external view returns (uint256);
 }
