fd66ec6e06b90de538318f3fe08a056bec6482d6
Piotr Rzonsowski <94194924+pete-ipor@users.noreply.github.com>
2024-03-26 08:21:07 +0100

    Feature/il 4233 balance aave v3 (#5)

---
+++ contracts/libraries/math/IporMath.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.20;
+
+library IporMath {
+    function convertToWad(int256 value, uint256 assetDecimals) internal pure returns (int256) {
+        if (value == 0) {
+            return 0;
+        }
+        if (assetDecimals == 18) {
+            return value;
+        } else if (assetDecimals > 18) {
+            return divisionInt(value, int256(10 ** (assetDecimals - 18)));
+        } else {
+            return value * int256(10 ** (18 - assetDecimals));
+        }
+    }
+
+    function divisionInt(int256 x, int256 y) internal pure returns (int256 z) {
+        uint256 absX = uint256(x < 0 ? -x : x);
+        uint256 absY = uint256(y < 0 ? -y : y);
+
+        // Use bitwise XOR to get the sign on MBS bit then shift to LSB
+        // sign == 0x0000...0000 ==  0 if the number is non-negative
+        // sign == 0xFFFF...FFFF == -1 if the number is negative
+        int256 sign = (x ^ y) >> 255;
+
+        uint256 divAbs;
+        uint256 remainder;
+
+        unchecked {
+            divAbs = absX / absY;
+            remainder = absX % absY;
+        }
+        // Check if we need to round
+        if (sign < 0) {
+            // remainder << 1 left shift is equivalent to multiplying by 2
+            if (remainder << 1 > absY) {
+                ++divAbs;
+            }
+        } else {
+            if (remainder << 1 >= absY) {
+                ++divAbs;
+            }
+        }
+
+        // (sign | 1) is cheaper than (sign < 0) ? -1 : 1;
+        unchecked {
+            z = int256(divAbs) * (sign | 1);
+        }
+    }
+}
