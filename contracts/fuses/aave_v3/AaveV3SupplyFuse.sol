849ecc8b7e15bd216d410061ecad5d9bb02bf02b
Mario <mario@ipor.io>
2024-04-09 14:57:12 +0200

    Additional tests and small refactor.

--- contracts/fuses/aave_v3/AaveV3SupplyFuse.sol
+++ contracts/fuses/aave_v3/AaveV3SupplyFuse.sol
@@ -11,13 +11,20 @@ import {MarketConfigurationLib} from "../../libraries/MarketConfigurationLib.sol
 contract AaveV3SupplyFuse is IFuse {
     using SafeCast for uint256;
 
-    struct AaveV3SupplyFuseData {
-        // token to supply
+    struct AaveV3SupplyFuseEnterData {
+        /// @notice asset address to supply
         address asset;
-        // max amount to supply
+        /// @notice asset amount to supply
         uint256 amount;
-        // user eMode category if pass value bigger than 255 is ignored and not set
+        /// @notice user eMode category if pass value bigger than 255 is ignored and not set
         uint256 userEModeCategoryId;
+    }
+
+    struct AaveV3SupplyFuseExitData {
+        /// @notice asset address to withdraw
+        address asset;
+        /// @notice asset amount to withdraw
+        uint256 amount;
     }
 
     address public immutable VERSION;
@@ -25,7 +32,8 @@ contract AaveV3SupplyFuse is IFuse {
 
     IPool public immutable AAVE_POOL;
 
-    event AaveV3SupplyFuse(address version, string action, address asset, uint256 amount, uint256 userEModeCategoryId);
+    event AaveV3SupplyEnterFuse(address version, address asset, uint256 amount, uint256 userEModeCategoryId);
+    event AaveV3SupplyExitFuse(address version, address asset, uint256 amount);
 
     error AaveV3SupplyFuseUnsupportedAsset(string action, address asset, string errorCode);
 
@@ -36,15 +44,14 @@ contract AaveV3SupplyFuse is IFuse {
     }
 
     function enter(bytes calldata data) external {
-        AaveV3SupplyFuseData memory structData = abi.decode(data, (AaveV3SupplyFuseData));
-        _enter(structData);
+        _enter(abi.decode(data, (AaveV3SupplyFuseEnterData)));
     }
 
-    function enter(AaveV3SupplyFuseData memory data) external {
+    function enter(AaveV3SupplyFuseEnterData memory data) external {
         _enter(data);
     }
 
-    function _enter(AaveV3SupplyFuseData memory data) internal {
+    function _enter(AaveV3SupplyFuseEnterData memory data) internal {
         if (!MarketConfigurationLib.isSubstrateAsAssetGranted(MARKET_ID, data.asset)) {
             revert AaveV3SupplyFuseUnsupportedAsset("enter", data.asset, Errors.UNSUPPORTED_ASSET);
         }
@@ -57,25 +64,24 @@ contract AaveV3SupplyFuse is IFuse {
             AAVE_POOL.setUserEMode(data.userEModeCategoryId.toUint8());
         }
 
-        emit AaveV3SupplyFuse(VERSION, "enter", data.asset, data.amount, data.userEModeCategoryId);
+        emit AaveV3SupplyEnterFuse(VERSION, data.asset, data.amount, data.userEModeCategoryId);
     }
 
     function exit(bytes calldata data) external {
-        AaveV3SupplyFuseData memory data = abi.decode(data, (AaveV3SupplyFuseData));
+        _exit(abi.decode(data, (AaveV3SupplyFuseExitData)));
+    }
+
+    function exit(AaveV3SupplyFuseExitData calldata data) external {
         _exit(data);
     }
 
-    function exit(AaveV3SupplyFuseData calldata data) external {
-        _exit(data);
-    }
-
-    function _exit(AaveV3SupplyFuseData memory data) internal {
+    function _exit(AaveV3SupplyFuseExitData memory data) internal {
         if (!MarketConfigurationLib.isSubstrateAsAssetGranted(MARKET_ID, data.asset)) {
             revert AaveV3SupplyFuseUnsupportedAsset("exit", data.asset, Errors.UNSUPPORTED_ASSET);
         }
 
         uint256 withdrawnAmount = AAVE_POOL.withdraw(data.asset, data.amount, address(this));
 
-        emit AaveV3SupplyFuse(VERSION, "exit", data.asset, withdrawnAmount, data.userEModeCategoryId);
+        emit AaveV3SupplyExitFuse(VERSION, data.asset, withdrawnAmount);
     }
 }
--- test/fuses/compound_v3/CompoundUsdcV3SupplyFuseTest.t.sol
+++ test/fuses/compound_v3/CompoundUsdcV3SupplyFuseTest.t.sol
@@ -42,7 +42,7 @@ contract CompoundUsdcV3SupplyFuseTest is Test {
 
         // when
 
-        fuseMock.enter(CompoundV3SupplyFuse.CompoundV3SupplyFuseData({asset: activeTokens.asset, amount: amount}));
+        fuseMock.enter(CompoundV3SupplyFuse.CompoundV3SupplyFuseEnterData({asset: activeTokens.asset, amount: amount}));
 
         // then
         uint256 balanceAfter = ERC20(activeTokens.asset).balanceOf(address(fuseMock));
@@ -66,14 +66,14 @@ contract CompoundUsdcV3SupplyFuseTest is Test {
         assets[0] = activeTokens.asset;
         fuseMock.grantAssetsToMarket(fuse.MARKET_ID(), assets);
 
-        fuseMock.enter(CompoundV3SupplyFuse.CompoundV3SupplyFuseData({asset: activeTokens.asset, amount: amount}));
+        fuseMock.enter(CompoundV3SupplyFuse.CompoundV3SupplyFuseEnterData({asset: activeTokens.asset, amount: amount}));
 
         uint256 balanceBefore = ERC20(activeTokens.asset).balanceOf(address(fuseMock));
         uint256 balanceOnCometBefore = _getBalance(address(fuseMock), activeTokens.asset);
 
         // when
         fuseMock.exit(
-            CompoundV3SupplyFuse.CompoundV3SupplyFuseData({asset: activeTokens.asset, amount: balanceOnCometBefore})
+            CompoundV3SupplyFuse.CompoundV3SupplyFuseExitData({asset: activeTokens.asset, amount: balanceOnCometBefore})
         );
 
         // then
