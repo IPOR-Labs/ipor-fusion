ebf52ff833c4f53736c0a265d85939f967a4affc
Mario <mario@ipor.io>
2024-04-10 16:12:17 +0200

    IL-4228 deposit tests

--- contracts/priceOracle/IIporPriceOracle.sol
+++ contracts/priceOracle/IIporPriceOracle.sol
@@ -2,15 +2,17 @@
 pragma solidity 0.8.20;
 
 interface IIporPriceOracle {
-    function setAssetSources(address[] calldata assets, address[] calldata sources) external;
-
     /// @notice Returns the price of the given asset in 8 decimals
     /// @return price of the asset in 8 decimals
     function getAssetPrice(address asset) external view returns (uint256);
 
+    /// @notice Returns the prices of the given assets in 8 decimals
+    /// @return prices of the assets in 8 decimals
     function getAssetsPrices(address[] calldata assets) external view returns (uint256[] memory);
 
     function getSourceOfAsset(address asset) external view returns (address);
+
+    function setAssetSources(address[] calldata assets, address[] calldata sources) external;
 
     //solhint-disable-next-line
     function BASE_CURRENCY() external view returns (address);
