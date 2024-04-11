0a168683b9dea8da923b96994a1becc89b11475e
Alvaro Serrano <alvaro@ipor.io>
2024-04-11 18:29:06 +0200

    ERC4626BalanceFuse fix int balance

--- contracts/fuses/erc4626/Erc4626BalanceFuse.sol
+++ contracts/fuses/erc4626/Erc4626BalanceFuse.sol
@@ -11,51 +11,45 @@ import {IporMath} from "../../libraries/math/IporMath.sol";
 import {Errors} from "./../../libraries/errors/Errors.sol";
 
 contract ERC4626BalanceFuse is IMarketBalanceFuse {
-	using SafeCast for uint256;
+    using SafeCast for uint256;
 
-	uint256 private constant PRICE_DECIMALS = 8;
+    uint256 private constant PRICE_DECIMALS = 8;
 
-	uint256 public immutable MARKET_ID;
-	IIporPriceOracle public immutable PRICE_ORACLE;
+    uint256 public immutable MARKET_ID;
+    IIporPriceOracle public immutable PRICE_ORACLE;
 
-	constructor(uint256 marketIdInput, address priceOracle) {
-		MARKET_ID = marketIdInput;
-		PRICE_ORACLE = priceOracle;
-	}
+    constructor(uint256 marketIdInput, address priceOracle) {
+        MARKET_ID = marketIdInput;
+        PRICE_ORACLE = priceOracle;
+    }
 
-	function balanceOf(address plazmaVault) external view override returns (uint256) {
-		bytes32[] memory vaults = MarketConfigurationLib.getMarketConfigurationSubstrates(MARKET_ID);
+    function balanceOf(address plazmaVault) external view override returns (uint256) {
+        bytes32[] memory vaults = MarketConfigurationLib.getMarketConfigurationSubstrates(MARKET_ID);
 
-		uint256 len = vaults.length;
+        uint256 len = vaults.length;
 
-		if (len == 0) {
-			return 0;
-		}
+        if (len == 0) {
+            return 0;
+        }
 
-		int256 balance = 0;
-		uint256 vaultShares;
-		uint256 vaultAssets;
-		uint256 decimals;
-		// @dev this value has 8 decimals
-		uint256 assetPrice;
-		address asset;
-		address vaultAddress;
+        uint256 balance = 0;
+        uint256 vaultAssets;
+        uint256 decimals;
+        // @dev this value has 8 decimals
+        uint256 assetPrice;
+        address vaultAddress;
+        IERC4626 vault;
 
-		for (uint256 i; i < len; ++i) {
-			vaultAddress = MarketConfigurationLib.bytes32ToAddress(vaults[i]);
-			IERC4626 vault = IERC4626(vaultAddress);
-			vaultShares = vault.balanceOf(plazmaVault);
-			vaultAssets = vault.convertToAssets(vaultShares);
-			asset = vault.asset();
-			decimals = IERC20(asset).decimals();
-			assetPrice = PRICE_ORACLE.getAssetPrice(asset);
-			balance += IporMath.convertToWadInt(
-				vaultAssets * assetPrice,
-				decimals + PRICE_DECIMALS
-			);
-		}
+        for (uint256 i; i < len; ++i) {
+            vaultAddress = MarketConfigurationLib.bytes32ToAddress(vaults[i]);
+            vault = IERC4626(vaultAddress);
+            vaultAssets = vault.convertToAssets(vault.balanceOf(plazmaVault));
+            balance += IporMath.convertToWad(
+                vaultAssets * PRICE_ORACLE.getAssetPrice(vault.asset()),
+                vault.decimals() + PRICE_DECIMALS
+            );
+        }
 
-		return balance.toUint256();
-	}
-
+        return balance;
+    }
 }
