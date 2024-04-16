85ecda999e2434ded84a5814473f1b0c03f18f6a
Piotr Rzonsowski <pete@ipor.io>
2024-04-16 13:32:27 +0200

    implementation

---
+++ contracts/fuses/compound_v2/CErc20.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.20;
+
+interface CErc20 {
+    function mint(uint256) external returns (uint256);
+
+    function exchangeRateCurrent() external returns (uint256);
+
+    function supplyRatePerBlock() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function exchangeRateStored() external view returns (uint256);
+
+    function redeem(uint256) external returns (uint256);
+
+    function decimals() external view returns (uint8);
+
+    function underlying() external view returns (address);
+
+    /**
+     * @notice Sender redeems cTokens in exchange for a specified amount of underlying asset
+     * @dev Accrues interest whether or not the operation succeeds, unless reverted
+     * @param redeemAmount The amount of underlying to redeem
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     */
+    function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
+
+    /**
+     * @notice Get the underlying balance of the `owner`
+     * @dev This also accrues interest in a transaction
+     * @param owner The address of the account to query
+     * @return The amount of underlying owned by `owner`
+     */
+    function balanceOfUnderlying(address owner) external returns (uint256);
+
+    /**
+     * @notice Accrue interest to updated borrowIndex and then calculate account's borrow balance using the updated borrowIndex
+     * @param account The address whose balance should be calculated after updating borrowIndex
+     * @return The calculated balance
+     */
+    function borrowBalanceCurrent(address account) external returns (uint256);
+}
---
+++ contracts/fuses/compound_v2/CompoundV2BalanceFuse.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.20;
+
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IMarketBalanceFuse} from "../IMarketBalanceFuse.sol";
+import {IporMath} from "../../libraries/math/IporMath.sol";
+import {MarketConfigurationLib} from "../../libraries/MarketConfigurationLib.sol";
+import {CErc20} from "./CErc20.sol";
+import {IIporPriceOracle} from "../../priceOracle/IIporPriceOracle.sol";
+
+contract CompoundV2BalanceFuse is IMarketBalanceFuse {
+    using SafeCast for int256;
+    using SafeCast for uint256;
+    using Address for address;
+
+    uint256 private constant PRICE_DECIMALS = 8;
+    uint256 public immutable MARKET_ID;
+    IIporPriceOracle public immutable PRICE_ORACLE;
+
+    constructor(uint256 marketIdInput, address priceOracle) {
+        MARKET_ID = marketIdInput;
+        PRICE_ORACLE = IIporPriceOracle(priceOracle);
+    }
+
+    function balanceOf(address plazmaVault) external override returns (uint256) {
+        bytes32[] memory assetsRaw = MarketConfigurationLib.getMarketConfigurationSubstrates(MARKET_ID);
+
+        uint256 len = assetsRaw.length;
+        if (len == 0) {
+            return 0;
+        }
+
+        int256 balanceTemp = 0;
+        int256 balanceInLoop;
+        uint256 decimals;
+        // @dev this value has 8 decimals
+        uint256 price;
+        CErc20 cToken;
+        int256 borrowBalance;
+        address underlying;
+        uint256 rawBalance;
+        uint256 rawBorrowBalance;
+
+        for (uint256 i; i < len; ++i) {
+            balanceInLoop = 0;
+            cToken = CErc20(MarketConfigurationLib.bytes32ToAddress(assetsRaw[i]));
+            underlying = cToken.underlying();
+            decimals = ERC20(underlying).decimals();
+            price = _getPrice(underlying);
+
+            rawBalance = cToken.balanceOfUnderlying(plazmaVault);
+            balanceTemp += IporMath.convertToWadInt(rawBalance.toInt256() * int256(price), decimals + PRICE_DECIMALS);
+            rawBorrowBalance = cToken.borrowBalanceCurrent(plazmaVault);
+            borrowBalance = IporMath.convertToWadInt((rawBorrowBalance * price).toInt256(), decimals + PRICE_DECIMALS);
+
+            balanceTemp -= borrowBalance;
+        }
+
+        return balanceTemp.toUint256();
+    }
+
+    function _getPrice(address asset) internal view returns (uint256) {
+        return PRICE_ORACLE.getAssetPrice(asset);
+    }
+}
