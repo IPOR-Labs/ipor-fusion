cb68f664ebc07660b8d845686add06d192e5704b
wookash <wookash@ipor.io>
2024-06-12 15:05:19 +0200

    licenses updated

--- contracts/fuses/erc4626/Erc4626SupplyFuse.sol
+++ contracts/fuses/erc4626/Erc4626SupplyFuse.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
