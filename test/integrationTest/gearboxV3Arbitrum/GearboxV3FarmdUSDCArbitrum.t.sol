4bdc80e4dbed88e2551e51e11c155a4dcdaf54d2
Piotr Rzonsowski <pete@ipor.io>
2024-07-23 05:45:59 +0200

    CR

--- test/integrationTest/gearboxV3Arbitrum/GearboxV3FarmdUSDCArbitrum.t.sol
+++ test/integrationTest/gearboxV3Arbitrum/GearboxV3FarmdUSDCArbitrum.t.sol
@@ -94,7 +94,7 @@ contract GearboxV3FarmdUSDCArbitrum is SupplyTest {
         Erc4626SupplyFuseEnterData memory enterData = Erc4626SupplyFuseEnterData({vault: D_USDC, amount: amount_});
         GearboxV3FarmdSupplyFuseEnterData memory enterDataFarm = GearboxV3FarmdSupplyFuseEnterData({
             farmdToken: FARM_D_USDC,
-            amount: amount_
+            dTokenAmount: amount_
         });
         data = new bytes[](2);
         data[0] = abi.encode(enterData);
@@ -110,7 +110,7 @@ contract GearboxV3FarmdUSDCArbitrum is SupplyTest {
         // TODO: should removed when the task https://ipor-labs.atlassian.net/browse/IL-4596 will be done.
         GearboxV3FarmdSupplyFuseExitData memory exitDataFarm = GearboxV3FarmdSupplyFuseExitData({
             farmdToken: FARM_D_USDC,
-            amount: amount_
+            dTokenAmount: amount_
         });
         data = new bytes[](2);
         data[0] = abi.encode(exitData);
