89ad2ba42ec702cc0140bf2556cf5659ca7ce37e
Piotr Rzonsowski <pete@ipor.io>
2024-08-05 12:25:07 +0200

    CR2

--- contracts/libraries/CallbackHandlerLib.sol
+++ contracts/libraries/CallbackHandlerLib.sol
@@ -32,6 +32,7 @@ library CallbackHandlerLib {
 
     function updateCallbackHandler(address handler_, address sender_, bytes4 sig_) internal {
         PlasmaVaultStorageLib.getCallbackHandler().callbackHandler[
+            /// @dev msg.sender - is the address of a contract which execute callback, msg.sig - is the signature of the function
             keccak256(abi.encodePacked(sender_, sig_))
         ] = handler_;
         emit CallbackHandlerUpdated(handler_, sender_, sig_);
