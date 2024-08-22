4b72c72c8be471be173f3803a3788eb4aca14ca7
Piotr Rzonsowski <pete@ipor.io>
2024-08-22 07:02:26 +0200

    add PRICE_ORACLE_MIDDLEWARE

--- contracts/fuses/erc20/Erc20BalanceFuse.sol
+++ contracts/fuses/erc20/Erc20BalanceFuse.sol
@@ -10,14 +10,19 @@ import {PlasmaVaultConfigLib} from "../../libraries/PlasmaVaultConfigLib.sol";
 import {IporMath} from "../../libraries/math/IporMath.sol";
 
 contract ERC20BalanceFuse is IMarketBalanceFuse {
-    uint256 private constant PRICE_DECIMALS = 8;
+    uint256 private constant PRICE_ORACLE_MIDDLEWARE_DECIMALS = 8;
     uint256 public immutable MARKET_ID;
 
-    IPriceOracleMiddleware public immutable PRICE_ORACLE;
+    IPriceOracleMiddleware public immutable PRICE_ORACLE_MIDDLEWARE;
 
     constructor(uint256 marketId_, address priceOracle_) {
         MARKET_ID = marketId_;
-        PRICE_ORACLE = IPriceOracleMiddleware(priceOracle_);
+        PRICE_ORACLE_MIDDLEWARE = IPriceOracleMiddleware(priceOracle_);
+        //TODO add validation after merge https://github.com/IPOR-Labs/ipor-fusion/pull/72
+        //        PRICE_ORACLE_MIDDLEWARE = IPriceOracleMiddleware(priceOracle_);
+        //        if (PRICE_ORACLE_MIDDLEWARE.QUOTE_CURRENCY_DECIMALS() != PRICE_ORACLE_MIDDLEWARE_DECIMALS) {
+        //            revert IPriceOracleMiddleware.WrongDecimals();
+        //        }
     }
 
     function balanceOf(address plasmaVault_) external view override returns (uint256) {
@@ -38,8 +43,8 @@ contract ERC20BalanceFuse is IMarketBalanceFuse {
                 continue;
             }
             balance += IporMath.convertToWad(
-                IERC20(token).balanceOf(plasmaVault_) * PRICE_ORACLE.getAssetPrice(token),
-                IERC20Metadata(token).decimals() + PRICE_DECIMALS
+                IERC20(token).balanceOf(plasmaVault_) * PRICE_ORACLE_MIDDLEWARE.getAssetPrice(token),
+                IERC20Metadata(token).decimals() + PRICE_ORACLE_MIDDLEWARE_DECIMALS
             );
         }
 
