716d76d1d529b8553a37b05090132515b7edd4e0
Alvaro Serrano <alvaro@ipor.io>
2024-08-22 19:03:17 +0200

    fix stack too deep

--- contracts/fuses/curve_gauge/CurveChildLiquidityGaugeBalanceFuse.sol
+++ contracts/fuses/curve_gauge/CurveChildLiquidityGaugeBalanceFuse.sol
@@ -44,7 +44,6 @@ contract CurveChildLiquidityGaugeBalanceFuse is IMarketBalanceFuse {
         uint256 withdrawTokenAmount; // underlying asset of the vault amount to withdraw from LP
         uint256 assetPrice;
         uint256 assetDecimals;
-        address vaultUnderlyingAsset = IERC4626(plasmaVault_).asset(); // Plasma Vault asset
         address stakedLpTokenAddress;
         address lpTokenAddress; // Curve LP token
         int128 indexCoin; // index of the underlying asset in the Curve pool
@@ -52,14 +51,14 @@ contract CurveChildLiquidityGaugeBalanceFuse is IMarketBalanceFuse {
         for (uint256 i; i < len; ++i) {
             stakedLpTokenAddress = PlasmaVaultConfigLib.bytes32ToAddress(substrates[i]);
             lpTokenAddress = IChildLiquidityGauge(stakedLpTokenAddress).lp_token();
-            indexCoin = _getCoinIndex(ICurveStableswapNG(lpTokenAddress), vaultUnderlyingAsset);
+            indexCoin = _getCoinIndex(ICurveStableswapNG(lpTokenAddress), IERC4626(plasmaVault_).asset());
             stakedLPTokenBalance = ERC20(stakedLpTokenAddress).balanceOf(plasmaVault_);
             if (stakedLPTokenBalance > 0) {
                 withdrawTokenAmount = ICurveStableswapNG(lpTokenAddress).calc_withdraw_one_coin(
                     stakedLPTokenBalance,
                     indexCoin
                 );
-                (assetPrice, assetDecimals) = PRICE_ORACLE.getAssetPrice(vaultUnderlyingAsset);
+                (assetPrice, assetDecimals) = PRICE_ORACLE.getAssetPrice(IERC4626(plasmaVault_).asset());
                 balance += IporMath.convertToWad(
                     withdrawTokenAmount * assetPrice,
                     ERC20(IERC4626(plasmaVault_).asset()).decimals() + assetDecimals
