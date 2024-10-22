91239103661ada495c56353597cae5ab0b88e6ad
Mario <mario@ipor.io>
2024-10-22 11:13:43 +0200

    Fix.

--- test/price_oracle/price_feed/RsETHAssetChainlinkPriceFeedArbitrumTest.t.sol
+++ test/price_oracle/price_feed/RsETHAssetChainlinkPriceFeedArbitrumTest.t.sol
@@ -5,7 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {AssetChainlinkPriceFeed} from "../../../contracts/price_oracle/price_feed/AssetChainlinkPriceFeed.sol";
 
 contract RsETHAssetChainlinkPriceFeedArbitrumTest is Test {
-    address public constant RS_ETH = 0xED65C5085a18Fa160Af0313E60dcc7905E944Dc7;
+    address public constant RS_ETH = 0x4186BFC76E2E237523CBC30FD220FE055156b41F;
     address public constant RS_ETH_ETH_CHAINLINKG_FEED = 0xb0EA543f9F8d4B818550365d13F66Da747e1476A;
     address public constant ETH_USD_CHAINLINKG_FEED = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
 
