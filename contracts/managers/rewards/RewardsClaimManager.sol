2cd00c66d247ab1c99c5dffd668a02839e1dbed5
Piotr Rzonsowski <pete@ipor.io>
2024-12-16 23:29:06 +0100

    RewardsClaimManager

--- contracts/managers/rewards/RewardsClaimManager.sol
+++ contracts/managers/rewards/RewardsClaimManager.sol
@@ -15,18 +15,30 @@ import {PlasmaVault} from "../../vaults/PlasmaVault.sol";
 import {IRewardsClaimManager} from "../../interfaces/IRewardsClaimManager.sol";
 import {ContextClient} from "../context/ContextClient.sol";
 
-/// @title RewardsClaimManager contract responsible for managing rewards claiming from the Plasma Vault
+/// @title RewardsClaimManager
+/// @notice Manages the claiming and vesting of rewards from the Plasma Vault
+/// @dev This contract implements role-based access control for various reward management functions
 contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IRewardsClaimManager {
     using SafeERC20 for IERC20;
     using SafeCast for uint256;
 
+    /// @notice The underlying token used for rewards
     address public immutable UNDERLYING_TOKEN;
+
+    /// @notice The address of the Plasma Vault contract
     address public immutable PLASMA_VAULT;
 
     error UnableToTransferUnderlyingToken();
 
+    /// @notice Emitted when rewards are withdrawn
+    /// @param amount The amount of tokens withdrawn
     event AmountWithdrawn(uint256 amount);
 
+    /// @notice Initializes the RewardsClaimManager
+    /// @param initialAuthority_ The initial authority address for access control
+    /// @param plasmaVault_ The address of the Plasma Vault contract
+    /// @dev Sets up initial vesting time and configures access control
+    /// @custom:access Only during initialization
     constructor(address initialAuthority_, address plasmaVault_) initializer {
         super.__AccessManaged_init_unchained(initialAuthority_);
 
@@ -36,6 +48,10 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         RewardsClaimManagersStorageLib.setupVestingTime(1);
     }
 
+    /// @notice Returns the current balance of vested tokens
+    /// @return The amount of tokens currently available for claiming
+    /// @dev Calculates vested amount based on vesting schedule
+    /// @custom:access Public
     function balanceOf() public view returns (uint256) {
         VestingData memory data = RewardsClaimManagersStorageLib.getVestingData();
 
@@ -63,18 +79,34 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         }
     }
 
+    /// @notice Checks if a given fuse is supported for rewards
+    /// @param fuse_ The address of the fuse to check
+    /// @return bool True if the fuse is supported
+    /// @custom:access Public
     function isRewardFuseSupported(address fuse_) external view returns (bool) {
         return FusesLib.isFuseSupported(fuse_);
     }
 
+    /// @notice Gets the current vesting data
+    /// @return VestingData struct containing vesting schedule information
+    /// @custom:access Public
     function getVestingData() external view returns (VestingData memory) {
         return RewardsClaimManagersStorageLib.getVestingData();
     }
 
+    /// @notice Returns array of supported reward fuses
+    /// @return Array of fuse addresses
+    /// @custom:access Public
     function getRewardsFuses() external view returns (address[] memory) {
         return FusesLib.getFusesArray();
     }
 
+    /// @notice Transfers tokens to a specified address
+    /// @param asset_ The token to transfer
+    /// @param to_ The recipient address
+    /// @param amount_ The amount to transfer
+    /// @dev Cannot transfer the underlying token
+    /// @custom:access TRANSFER_REWARDS_ROLE
     function transfer(address asset_, address to_, uint256 amount_) external restricted {
         if (asset_ == UNDERLYING_TOKEN) {
             revert UnableToTransferUnderlyingToken();
@@ -82,6 +114,9 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         IERC20(asset_).safeTransfer(to_, amount_);
     }
 
+    /// @notice Claims rewards from supported fuses
+    /// @param calls_ Array of FuseAction structs defining claim operations
+    /// @custom:access CLAIM_REWARDS_ROLE
     function claimRewards(FuseAction[] calldata calls_) external restricted {
         uint256 len = calls_.length;
         for (uint256 i; i < len; ++i) {
@@ -93,6 +128,9 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         PlasmaVault(PLASMA_VAULT).claimRewards(calls_);
     }
 
+    /// @notice Updates the balance and vesting schedule
+    /// @dev Transfers available tokens to Plasma Vault and updates vesting data
+    /// @custom:access PUBLIC_ROLE
     function updateBalance() external restricted {
         uint256 balance = balanceOf();
         if (balance > 0) {
@@ -106,6 +144,9 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         RewardsClaimManagersStorageLib.setVestingData(data);
     }
 
+    /// @notice Transfers vested tokens to the Plasma Vault
+    /// @dev Moves available vested tokens and updates accounting
+    /// @custom:access PUBLIC_ROLE
     function transferVestedTokensToVault() external restricted {
         uint256 balance = balanceOf();
         if (balance == 0) {
@@ -116,6 +157,9 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         emit AmountWithdrawn(balance);
     }
 
+    /// @notice Adds new reward fuses
+    /// @param fuses_ Array of fuse addresses to add
+    /// @custom:access FUSE_MANAGER_ROLE
     function addRewardFuses(address[] calldata fuses_) external restricted {
         uint256 len = fuses_.length;
         for (uint256 i; i < len; ++i) {
@@ -123,6 +167,9 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         }
     }
 
+    /// @notice Removes reward fuses
+    /// @param fuses_ Array of fuse addresses to remove
+    /// @custom:access FUSE_MANAGER_ROLE
     function removeRewardFuses(address[] calldata fuses_) external restricted {
         uint256 len = fuses_.length;
         for (uint256 i; i < len; ++i) {
@@ -130,18 +177,24 @@ contract RewardsClaimManager is AccessManagedUpgradeable, ContextClient, IReward
         }
     }
 
+    /// @notice Sets the vesting duration
+    /// @param vestingTime_ The new vesting duration in seconds
+    /// @custom:access ATOMIST_ROLE
     function setupVestingTime(uint256 vestingTime_) external restricted {
         RewardsClaimManagersStorageLib.setupVestingTime(vestingTime_);
     }
 
+    /// @notice Internal function to get the message sender from context
+    /// @return The address of the message sender
     function _msgSender() internal view override returns (address) {
         return getSenderFromContext();
     }
 
-    /**
-     * @dev Reverts if the caller is not allowed to call the function identified by a selector. Panics if the calldata
-     * is less than 4 bytes long.
-     */
+    /// @notice Checks if a caller can execute a function
+    /// @param caller_ The address attempting to call the function
+    /// @param data_ The calldata of the function call
+    /// @dev Handles special cases for context management functions
+    /// @custom:access Internal
     function _checkCanCall(address caller_, bytes calldata data_) internal override {
         bytes4 sig = bytes4(data_[0:4]);
         // @dev for context manager 87ef0b87 - setupContext, db99bddd - clearContext
