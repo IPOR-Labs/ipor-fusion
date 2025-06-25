b473a7af533fd2cc059728476e51f8090e049611
Mario <mario@ipor.io>
2025-06-25 11:36:52 +0200

    WrappedPlasmaVault Factory

---
+++ contracts/factory/extensions/WrappedPlasmaVaultFactory.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {WrappedPlasmaVault} from "../../vaults/extensions/WrappedPlasmaVault.sol";
+
+/// @title WrappedPlasmaVaultFactory
+/// @notice Factory contract for creating wrapped plasma vaults
+/// @dev This contract is upgradeable and uses UUPS pattern for upgrades
+contract WrappedPlasmaVaultFactory is UUPSUpgradeable, Ownable2StepUpgradeable {
+    /// @notice Error thrown when an invalid address (zero address) is provided
+    error InvalidAddress();
+
+    /// @notice Emitted when a new wrapped plasma vault is created
+    /// @param name The name of the wrapped plasma vault
+    /// @param symbol The symbol of the wrapped plasma vault
+    /// @param plasmaVault The address of the underlying plasma vault
+    event WrappedPlasmaVaultCreated(string name, string symbol, address plasmaVault, address wrappedPlasmaVault);
+
+    /// @notice Initializes the factory contract
+    /// @dev This function can only be called once during contract deployment
+    /// @param initialFactoryAdmin_ The address that will be set as the initial admin of the factory
+    function initialize(address initialFactoryAdmin_) external initializer {
+        if (initialFactoryAdmin_ == address(0)) revert InvalidAddress();
+        __Ownable_init(initialFactoryAdmin_);
+        __Ownable2Step_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function create(
+        string memory name_,
+        string memory symbol_,
+        address plasmaVault_
+    ) external returns (address wrappedPlasmaVault) {
+        wrappedPlasmaVault = address(new WrappedPlasmaVault(name_, symbol_, plasmaVault_));
+        emit WrappedPlasmaVaultCreated(name_, symbol_, plasmaVault_, wrappedPlasmaVault);
+    }
+
+    /// @notice Authorizes an upgrade to a new implementation
+    /// @dev Required by the OZ UUPS module, can only be called by the owner
+    /// @param newImplementation Address of the new implementation
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
---
+++ test/factory/WrappedPlasmaVaultFactory.t.sol
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {WrappedPlasmaVaultFactory} from "../../contracts/factory/extensions/WrappedPlasmaVaultFactory.sol";
+import {WrappedPlasmaVault} from "../../contracts/vaults/extensions/WrappedPlasmaVault.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {MockERC20} from "../test_helpers/MockERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Simple mock for PlasmaVault that implements the required interface
+contract MockPlasmaVault {
+    IERC20 public immutable asset;
+
+    constructor(address _asset) {
+        asset = IERC20(_asset);
+    }
+
+    function maxWithdraw(address) external view returns (uint256) {
+        return 1000e18; // Return a fixed amount for testing
+    }
+}
+
+contract WrappedPlasmaVaultFactoryTest is Test {
+    WrappedPlasmaVaultFactory public factory;
+    WrappedPlasmaVaultFactory public factoryImplementation;
+    MockPlasmaVault public plasmaVault;
+    MockERC20 public underlyingToken;
+
+    address public owner;
+    address public user;
+    address public otherUser;
+
+    string public constant VAULT_NAME = "Wrapped Test Vault";
+    string public constant VAULT_SYMBOL = "wTEST";
+
+    event WrappedPlasmaVaultCreated(string name, string symbol, address plasmaVault);
+
+    function setUp() public {
+        // Deploy mock token
+        underlyingToken = new MockERC20("Test Token", "TEST", 18);
+
+        // Deploy mock plasma vault
+        plasmaVault = new MockPlasmaVault(address(underlyingToken));
+
+        // Deploy factory implementation and proxy
+        factoryImplementation = new WrappedPlasmaVaultFactory();
+
+        owner = makeAddr("owner");
+        user = makeAddr("user");
+        otherUser = makeAddr("otherUser");
+
+        bytes memory initData = abi.encodeWithSignature("initialize(address)", owner);
+        factory = WrappedPlasmaVaultFactory(address(new ERC1967Proxy(address(factoryImplementation), initData)));
+    }
+
+    function testShouldRevertInitializeWithZeroAddress() public {
+        // Deploy new factory implementation
+        WrappedPlasmaVaultFactory newFactoryImpl = new WrappedPlasmaVaultFactory();
+
+        // Should revert when initializing with zero address
+        bytes memory initData = abi.encodeWithSignature("initialize(address)", address(0));
+        vm.expectRevert(WrappedPlasmaVaultFactory.InvalidAddress.selector);
+        new ERC1967Proxy(address(newFactoryImpl), initData);
+    }
+
+    function testShouldRevertInitializeTwice() public {
+        vm.expectRevert(abi.encodeWithSignature("InvalidInitialization()"));
+        factory.initialize(owner);
+    }
+
+    function testShouldCreateWrappedPlasmaVault() public {
+        // When
+
+        address wrappedVault = factory.create(VAULT_NAME, VAULT_SYMBOL, address(plasmaVault));
+
+        // Then
+        assertTrue(wrappedVault != address(0), "Wrapped vault should be created");
+
+        WrappedPlasmaVault vault = WrappedPlasmaVault(wrappedVault);
+        assertEq(vault.name(), VAULT_NAME, "Vault name should be correct");
+        assertEq(vault.symbol(), VAULT_SYMBOL, "Vault symbol should be correct");
+        assertEq(vault.PLASMA_VAULT(), address(plasmaVault), "Vault plasma vault should be correct");
+    }
+
+    function testShouldCreateMultipleWrappedPlasmaVaults() public {
+        // Create first vault
+        address wrappedVault1 = factory.create(VAULT_NAME, VAULT_SYMBOL, address(plasmaVault));
+
+        // Create second vault with different parameters
+        address wrappedVault2 = factory.create("Second Vault", "wSEC", address(plasmaVault));
+
+        // Verify both vaults are different
+        assertTrue(wrappedVault1 != wrappedVault2, "Vaults should be different addresses");
+
+        WrappedPlasmaVault vault1 = WrappedPlasmaVault(wrappedVault1);
+        WrappedPlasmaVault vault2 = WrappedPlasmaVault(wrappedVault2);
+
+        assertEq(vault1.name(), VAULT_NAME);
+        assertEq(vault2.name(), "Second Vault");
+        assertEq(vault1.symbol(), VAULT_SYMBOL);
+        assertEq(vault2.symbol(), "wSEC");
+    }
+
+    function testShouldAllowCreate() public {
+        // Then: should be able to create vault
+        address wrappedVault = factory.create(VAULT_NAME, VAULT_SYMBOL, address(plasmaVault));
+        assertTrue(wrappedVault != address(0), "Should be able to create vault after unpause");
+    }
+
+    function testShouldRevertUpgradeByNonOwner() public {
+        // Deploy new implementation
+        WrappedPlasmaVaultFactory newImplementation = new WrappedPlasmaVaultFactory();
+
+        // Should revert when non-owner tries to upgrade
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSignature("OwnableUnauthorizedAccount(address)", user));
+        factory.upgradeToAndCall(address(newImplementation), "");
+    }
+
+    function testShouldUpgradeByOwner() public {
+        // Deploy new implementation
+        WrappedPlasmaVaultFactory newImplementation = new WrappedPlasmaVaultFactory();
+
+        // Should succeed when owner upgrades
+        vm.prank(owner);
+        factory.upgradeToAndCall(address(newImplementation), "");
+
+        // Verify upgrade was successful by checking that we can still call functions
+        address wrappedVault = factory.create(VAULT_NAME, VAULT_SYMBOL, address(plasmaVault));
+        assertTrue(wrappedVault != address(0), "Should be able to create vault after upgrade");
+    }
+
+    function testShouldTransferOwnership() public {
+        // Test ownership transfer
+        vm.prank(owner);
+        factory.transferOwnership(user);
+
+        // Accept ownership
+        vm.prank(user);
+        factory.acceptOwnership();
+
+        assertEq(factory.owner(), user, "Ownership should be transferred");
+    }
+
+    function testShouldRevertCreateWithZeroPlasmaVaultAddress() public {
+        // Should revert when creating vault with zero plasma vault address
+        vm.expectRevert();
+        factory.create(VAULT_NAME, VAULT_SYMBOL, address(0));
+    }
+
+    function testShouldCreateVaultWithEmptyStrings() public {
+        // Should be able to create vault with empty name/symbol (though not recommended)
+        address wrappedVault = factory.create("", "", address(plasmaVault));
+        assertTrue(wrappedVault != address(0), "Should be able to create vault with empty strings");
+
+        WrappedPlasmaVault vault = WrappedPlasmaVault(wrappedVault);
+        assertEq(vault.name(), "");
+        assertEq(vault.symbol(), "");
+    }
+
+    function testShouldCreateVaultWithLongStrings() public {
+        // Should be able to create vault with long name/symbol
+        string memory longName = "This is a very long name for testing purposes with many characters";
+        string memory longSymbol = "VERYLONGSYMBOL";
+
+        address wrappedVault = factory.create(longName, longSymbol, address(plasmaVault));
+        assertTrue(wrappedVault != address(0), "Should be able to create vault with long strings");
+
+        WrappedPlasmaVault vault = WrappedPlasmaVault(wrappedVault);
+        assertEq(vault.name(), longName);
+        assertEq(vault.symbol(), longSymbol);
+    }
+
+    function testShouldMaintainStateAfterUpgrade() public {
+        // Create a vault before upgrade
+        address wrappedVault = factory.create(VAULT_NAME, VAULT_SYMBOL, address(plasmaVault));
+
+        // Deploy new implementation and upgrade
+        WrappedPlasmaVaultFactory newImplementation = new WrappedPlasmaVaultFactory();
+        vm.prank(owner);
+        factory.upgradeToAndCall(address(newImplementation), "");
+
+        // Verify the created vault still exists and has correct properties
+        WrappedPlasmaVault vault = WrappedPlasmaVault(wrappedVault);
+        assertEq(vault.name(), VAULT_NAME);
+        assertEq(vault.symbol(), VAULT_SYMBOL);
+        assertEq(vault.PLASMA_VAULT(), address(plasmaVault));
+
+        // Verify factory can still create new vaults
+        address newWrappedVault = factory.create("New Vault", "wNEW", address(plasmaVault));
+        assertTrue(newWrappedVault != address(0), "Should be able to create new vault after upgrade");
+    }
+
+    function testShouldRevertCreateWhenPlasmaVaultIsInvalid() public {
+        // Should revert when plasma vault address is not a contract
+        vm.expectRevert();
+        factory.create(VAULT_NAME, VAULT_SYMBOL, user);
+    }
+}
