ef17c5b8ee3469d1b155b9722c0f10aa410b93ac
Piotr Rzonsowski <pete@ipor.io>
2025-10-30 11:09:53 +0100

    Add tests

--- test/vaults/extensions/WhitelistWrappedPlasmaVaultAccessTest.t.sol
+++ test/vaults/extensions/WhitelistWrappedPlasmaVaultAccessTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.26;
 import {Test} from "forge-std/Test.sol";
 import {WhitelistWrappedPlasmaVault} from "../../../contracts/vaults/extensions/WhitelistWrappedPlasmaVault.sol";
 import {PlasmaVault} from "../../../contracts/vaults/PlasmaVault.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract WhitelistWrappedPlasmaVaultAccessTest is Test {
     WhitelistWrappedPlasmaVault public wPlasmaVault;
@@ -114,5 +115,234 @@ contract WhitelistWrappedPlasmaVaultAccessTest is Test {
         vm.stopPrank();
 
         assertFalse(wPlasmaVault.hasRole(whitelistManager, newManager), "newManager should not gain WHITELIST_MANAGER");
+    }
+
+    function testNonManagerCannotGrantWhitelisted() public {
+        bytes32 whitelistManager = wPlasmaVault.WHITELIST_MANAGER();
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+        address attacker = makeAddr("attacker3");
+        address newWhitelisted = makeAddr("newWhitelisted");
+
+        // attacker has neither DEFAULT_ADMIN_ROLE nor WHITELIST_MANAGER
+        vm.startPrank(attacker);
+        vm.expectRevert(
+            abi.encodeWithSignature("AccessControlUnauthorizedAccount(address,bytes32)", attacker, whitelistManager)
+        );
+        wPlasmaVault.grantRole(whitelisted, newWhitelisted);
+        vm.stopPrank();
+
+        assertFalse(wPlasmaVault.hasRole(whitelisted, newWhitelisted), "newWhitelisted should not gain WHITELISTED");
+    }
+
+    function testWhitelistedUserCanDeposit() public {
+        bytes32 whitelistManager = wPlasmaVault.WHITELIST_MANAGER();
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+
+        // grant roles: admin -> manager (WHITELIST_MANAGER), manager -> user (WHITELISTED)
+        vm.prank(admin);
+        wPlasmaVault.grantRole(whitelistManager, manager);
+        vm.prank(manager);
+        wPlasmaVault.grantRole(whitelisted, user);
+
+        // fund user with USDC and approve vault
+        uint256 assets = 1_000e6; // 1,000 USDC
+        deal(USDC, user, assets);
+        vm.startPrank(user);
+        IERC20(USDC).approve(address(wPlasmaVault), assets);
+
+        uint256 shares = wPlasmaVault.deposit(assets, user);
+        vm.stopPrank();
+
+        assertGt(shares, 0, "deposit should mint shares");
+        assertEq(wPlasmaVault.balanceOf(user), shares, "user balance should equal minted shares");
+    }
+
+    function testWhitelistedUserCanMint() public {
+        bytes32 whitelistManager = wPlasmaVault.WHITELIST_MANAGER();
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+
+        // grant roles
+        vm.prank(admin);
+        wPlasmaVault.grantRole(whitelistManager, manager);
+        vm.prank(manager);
+        wPlasmaVault.grantRole(whitelisted, user);
+
+        // choose shares and fund user sufficiently, approve
+        uint256 sharesToMint = 1e9; // modest amount of shares
+        deal(USDC, user, 1_000_000e6);
+        vm.startPrank(user);
+        IERC20(USDC).approve(address(wPlasmaVault), type(uint256).max);
+
+        uint256 assetsSpent = wPlasmaVault.mint(sharesToMint, user);
+        vm.stopPrank();
+
+        assertGt(assetsSpent, 0, "mint should spend assets");
+        assertEq(wPlasmaVault.balanceOf(user), sharesToMint, "user should receive requested shares");
+    }
+
+    function testNonWhitelistedCannotDeposit() public {
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+        address attacker = makeAddr("attacker4");
+
+        uint256 assets = 1_000e6;
+        deal(USDC, attacker, assets);
+
+        vm.startPrank(attacker);
+        IERC20(USDC).approve(address(wPlasmaVault), assets);
+        vm.expectRevert(
+            abi.encodeWithSignature("AccessControlUnauthorizedAccount(address,bytes32)", attacker, whitelisted)
+        );
+        wPlasmaVault.deposit(assets, attacker);
+        vm.stopPrank();
+    }
+
+    function testNonWhitelistedCannotMint() public {
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+        address attacker = makeAddr("attacker5");
+
+        uint256 sharesToMint = 1e9;
+        deal(USDC, attacker, 1_000_000e6);
+
+        vm.startPrank(attacker);
+        IERC20(USDC).approve(address(wPlasmaVault), type(uint256).max);
+        vm.expectRevert(
+            abi.encodeWithSignature("AccessControlUnauthorizedAccount(address,bytes32)", attacker, whitelisted)
+        );
+        wPlasmaVault.mint(sharesToMint, attacker);
+        vm.stopPrank();
+    }
+
+    function testWhitelistedUserCanWithdraw() public {
+        bytes32 whitelistManager = wPlasmaVault.WHITELIST_MANAGER();
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+
+        // grant roles and deposit first
+        vm.prank(admin);
+        wPlasmaVault.grantRole(whitelistManager, manager);
+        vm.prank(manager);
+        wPlasmaVault.grantRole(whitelisted, user);
+
+        uint256 assets = 10_000e6;
+        deal(USDC, user, assets);
+
+        vm.startPrank(user);
+        IERC20(USDC).approve(address(wPlasmaVault), assets);
+        wPlasmaVault.deposit(assets, user);
+
+        vm.warp(block.timestamp + 10);
+
+        uint256 userUsdcBefore = IERC20(USDC).balanceOf(user);
+        uint256 userSharesBefore = wPlasmaVault.balanceOf(user);
+
+        uint256 toWithdraw = assets / 2;
+        uint256 sharesBurned = wPlasmaVault.withdraw(toWithdraw, user, user);
+        vm.stopPrank();
+
+        assertGt(sharesBurned, 0, "withdraw should burn shares");
+        assertLt(wPlasmaVault.balanceOf(user), userSharesBefore, "shares should decrease");
+        assertGt(IERC20(USDC).balanceOf(user), userUsdcBefore, "USDC balance should increase");
+    }
+
+    function testWhitelistedUserCanRedeem() public {
+        bytes32 whitelistManager = wPlasmaVault.WHITELIST_MANAGER();
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+
+        // grant roles and deposit first
+        vm.prank(admin);
+        wPlasmaVault.grantRole(whitelistManager, manager);
+        vm.prank(manager);
+        wPlasmaVault.grantRole(whitelisted, user);
+
+        uint256 assets = 10_000e6;
+        deal(USDC, user, assets);
+
+        vm.startPrank(user);
+        IERC20(USDC).approve(address(wPlasmaVault), assets);
+        wPlasmaVault.deposit(assets, user);
+
+        vm.warp(block.timestamp + 10);
+
+        uint256 userShares = wPlasmaVault.balanceOf(user);
+        uint256 userUsdcBefore = IERC20(USDC).balanceOf(user);
+
+        uint256 sharesToRedeem = userShares / 2;
+        uint256 assetsReceived = wPlasmaVault.redeem(sharesToRedeem, user, user);
+        vm.stopPrank();
+
+        assertGt(assetsReceived, 0, "redeem should return assets");
+        assertEq(wPlasmaVault.balanceOf(user), userShares - sharesToRedeem, "shares should reduce by redeemed");
+        assertGt(IERC20(USDC).balanceOf(user), userUsdcBefore, "USDC balance should increase");
+    }
+
+    function testTransferredSharesToNonWhitelistedCannotWithdraw() public {
+        bytes32 whitelistManager = wPlasmaVault.WHITELIST_MANAGER();
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+
+        address receiver = makeAddr("receiver");
+
+        // grant roles and deposit with whitelisted user
+        vm.prank(admin);
+        wPlasmaVault.grantRole(whitelistManager, manager);
+        vm.prank(manager);
+        wPlasmaVault.grantRole(whitelisted, user);
+
+        uint256 assets = 10_000e6;
+        deal(USDC, user, assets);
+
+        vm.startPrank(user);
+        IERC20(USDC).approve(address(wPlasmaVault), assets);
+        wPlasmaVault.deposit(assets, user);
+        vm.stopPrank();
+
+        // transfer some shares to non-whitelisted address
+        uint256 userShares = wPlasmaVault.balanceOf(user);
+        uint256 transferredShares = userShares / 2;
+        vm.prank(user);
+        wPlasmaVault.transfer(receiver, transferredShares);
+
+        uint256 assetsToWithdraw = wPlasmaVault.convertToAssetsWithFees(transferredShares);
+
+        // receiver attempts to withdraw -> should revert due to missing WHITELISTED role
+        vm.startPrank(receiver);
+        vm.expectRevert(
+            abi.encodeWithSignature("AccessControlUnauthorizedAccount(address,bytes32)", receiver, whitelisted)
+        );
+        wPlasmaVault.withdraw(assetsToWithdraw, receiver, receiver);
+        vm.stopPrank();
+    }
+
+    function testTransferredSharesToNonWhitelistedCannotRedeem() public {
+        bytes32 whitelistManager = wPlasmaVault.WHITELIST_MANAGER();
+        bytes32 whitelisted = wPlasmaVault.WHITELISTED();
+
+        address receiver = makeAddr("receiver2XXXXX");
+
+        // grant roles and deposit with whitelisted user
+        vm.prank(admin);
+        wPlasmaVault.grantRole(whitelistManager, manager);
+        vm.prank(manager);
+        wPlasmaVault.grantRole(whitelisted, user);
+
+        uint256 assets = 10_000e6;
+        deal(USDC, user, assets);
+
+        vm.startPrank(user);
+        IERC20(USDC).approve(address(wPlasmaVault), assets);
+        wPlasmaVault.deposit(assets, user);
+        vm.stopPrank();
+
+        // transfer some shares to non-whitelisted address
+        uint256 userShares = wPlasmaVault.balanceOf(user);
+        uint256 transferredShares = userShares / 2;
+        vm.prank(user);
+        wPlasmaVault.transfer(receiver, transferredShares);
+
+        // receiver attempts to redeem -> should revert due to missing WHITELISTED role
+        vm.startPrank(receiver);
+        vm.expectRevert(
+            abi.encodeWithSignature("AccessControlUnauthorizedAccount(address,bytes32)", receiver, whitelisted)
+        );
+        wPlasmaVault.redeem(transferredShares, receiver, receiver);
+        vm.stopPrank();
     }
 }
