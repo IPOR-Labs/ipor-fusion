ce78c208c9dbae3a8812b92e7958330c98206c4f
onibakuchi <etherdao0x00@gmail.com>
2025-11-20 14:22:51 +0700

    constant

--- contracts/fuses/napier/utils/Constants.sol
+++ contracts/fuses/napier/utils/Constants.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.10;
 
 library Constants {
     address constant NATIVE_TOKEN = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+    uint256 constant TOKI_SWAP_FEE_SCALE = 1e8;
+    uint16 constant BASIS_POINTS = 10_000;
 }
