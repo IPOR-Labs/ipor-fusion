56a4eb51e85c02ae37ae7e688822bb71b76be7d2
Piotr Rzonsowski <pete@ipor.io>
2025-12-02 09:23:08 +0100

    morpho flashloan

--- contracts/fuses/morpho/MorphoFlashLoanFuse.sol
+++ contracts/fuses/morpho/MorphoFlashLoanFuse.sol
@@ -8,6 +8,8 @@ import {IFuseCommon} from "../IFuseCommon.sol";
 import {PlasmaVaultConfigLib} from "../../libraries/PlasmaVaultConfigLib.sol";
 import {IMorpho} from "@morpho-org/morpho-blue/src/interfaces/IMorpho.sol";
 import {CallbackData} from "../../libraries/CallbackHandlerLib.sol";
+import {TransientStorageLib} from "../../transient_storage/TransientStorageLib.sol";
+import {TypeConversionLib} from "../../libraries/TypeConversionLib.sol";
 
 /// @dev Struct to hold data for entering a Morpho Flash Loan Fuse
 struct MorphoFlashLoanFuseEnterData {
@@ -37,9 +39,12 @@ contract MorphoFlashLoanFuse is IFuseCommon {
         MORPHO = IMorpho(morpho_);
     }
 
-    function enter(MorphoFlashLoanFuseEnterData calldata data_) external {
+    function enter(MorphoFlashLoanFuseEnterData memory data_) public returns (address asset, uint256 amount) {
         if (data_.tokenAmount == 0) {
-            return;
+            asset = data_.token;
+            amount = 0;
+            emit MorphoFlashLoanFuseEvent(VERSION, asset, amount);
+            return (asset, amount);
         }
 
         if (!PlasmaVaultConfigLib.isSubstrateAsAssetGranted(MARKET_ID, data_.token)) {
@@ -63,6 +68,45 @@ contract MorphoFlashLoanFuse is IFuseCommon {
 
         ERC20(data_.token).forceApprove(address(MORPHO), 0);
 
-        emit MorphoFlashLoanFuseEvent(VERSION, data_.token, data_.tokenAmount);
+        asset = data_.token;
+        amount = data_.tokenAmount;
+
+        emit MorphoFlashLoanFuseEvent(VERSION, asset, amount);
+    }
+
+    /// @notice Enters the Fuse using transient storage for parameters
+    function enterTransient() external {
+        bytes32[] memory inputs = TransientStorageLib.getInputs(VERSION);
+
+        address token = TypeConversionLib.toAddress(inputs[0]);
+        uint256 tokenAmount = TypeConversionLib.toUint256(inputs[1]);
+        uint256 callbackDataLength = TypeConversionLib.toUint256(inputs[2]);
+
+        bytes memory callbackFuseActionsData;
+        if (callbackDataLength > 0) {
+            callbackFuseActionsData = new bytes(callbackDataLength);
+            uint256 chunksCount = (callbackDataLength + 31) / 32; // ceil(callbackDataLength / 32)
+            for (uint256 i; i < chunksCount; ++i) {
+                bytes32 chunk = inputs[3 + i];
+                uint256 chunkStart = i * 32;
+                assembly {
+                    let dataPtr := add(add(callbackFuseActionsData, 0x20), chunkStart)
+                    mstore(dataPtr, chunk)
+                }
+            }
+        }
+
+        (address returnedAsset, uint256 returnedAmount) = enter(
+            MorphoFlashLoanFuseEnterData({
+                token: token,
+                tokenAmount: tokenAmount,
+                callbackFuseActionsData: callbackFuseActionsData
+            })
+        );
+
+        bytes32[] memory outputs = new bytes32[](2);
+        outputs[0] = TypeConversionLib.toBytes32(returnedAsset);
+        outputs[1] = TypeConversionLib.toBytes32(returnedAmount);
+        TransientStorageLib.setOutputs(VERSION, outputs);
     }
 }
