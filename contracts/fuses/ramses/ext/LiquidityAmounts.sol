a43395c23ffa6413de353d2e846c50c24fcbc4a5
Mario <85162486+mario-ipor@users.noreply.github.com>
2026-01-22 17:25:40 +0100

    IL-6805 R3.L2: Unchecked uint256→uint128 cast in exitTransient can silently truncate liquidity (#455)

--- contracts/fuses/ramses/ext/LiquidityAmounts.sol
+++ contracts/fuses/ramses/ext/LiquidityAmounts.sol
@@ -1,19 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.5.0;
+pragma solidity 0.8.30;
 
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "./FullMath.sol";
 import "@uniswap/v3-core/contracts/libraries/FixedPoint96.sol";
 
 /// @title Liquidity amount functions
 /// @notice Provides functions for computing liquidity amounts from token amounts and prices
 library LiquidityAmounts {
-    /// @notice Downcasts uint256 to uint128
-    /// @param x The uint258 to be downcasted
-    /// @return y The passed value, downcasted to uint128
-    function toUint128(uint256 x) internal pure returns (uint128 y) {
-        require((y = uint128(x)) == x);
-    }
-
     /// @notice Computes the amount of liquidity received for a given amount of token0 and price range
     /// @dev Calculates amount0 * (sqrt(upper) * sqrt(lower)) / (sqrt(upper) - sqrt(lower))
     /// @param sqrtRatioAX96 A sqrt price representing the first tick boundary
@@ -27,7 +21,7 @@ library LiquidityAmounts {
     ) internal pure returns (uint128 liquidity) {
         if (sqrtRatioAX96 > sqrtRatioBX96) (sqrtRatioAX96, sqrtRatioBX96) = (sqrtRatioBX96, sqrtRatioAX96);
         uint256 intermediate = FullMath.mulDiv(sqrtRatioAX96, sqrtRatioBX96, FixedPoint96.Q96);
-        return toUint128(FullMath.mulDiv(amount0, intermediate, sqrtRatioBX96 - sqrtRatioAX96));
+        return SafeCast.toUint128(FullMath.mulDiv(amount0, intermediate, sqrtRatioBX96 - sqrtRatioAX96));
     }
 
     /// @notice Computes the amount of liquidity received for a given amount of token1 and price range
@@ -42,7 +36,7 @@ library LiquidityAmounts {
         uint256 amount1
     ) internal pure returns (uint128 liquidity) {
         if (sqrtRatioAX96 > sqrtRatioBX96) (sqrtRatioAX96, sqrtRatioBX96) = (sqrtRatioBX96, sqrtRatioAX96);
-        return toUint128(FullMath.mulDiv(amount1, FixedPoint96.Q96, sqrtRatioBX96 - sqrtRatioAX96));
+        return SafeCast.toUint128(FullMath.mulDiv(amount1, FixedPoint96.Q96, sqrtRatioBX96 - sqrtRatioAX96));
     }
 
     /// @notice Computes the maximum amount of liquidity received for a given amount of token0, token1, the current
